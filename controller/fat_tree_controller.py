bd586c6380aa6429597bbd6d613028d1b75cf1f3
Franco Liberali <franco.liberali@gmail.com>
2020-07-13 00:31:03 -0300

    host_tracker and better logs

--- controller/fat_tree_controller.py
+++ controller/fat_tree_controller.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from pox.core import core
 import pox.openflow.discovery
+import pox.host_tracker
 from pox.lib.util import dpid_to_str
 
 log = core.getLogger()
@@ -8,23 +9,38 @@ log = core.getLogger()
 class FatTreeController:
 
     def __init__(self):
-        core.call_when_ready(self.startup, ('openflow', 'openflow_discovery'))
+        core.call_when_ready(
+            self.startup, ('openflow', 'openflow_discovery', 'host_tracker'))
 
     def startup(self):
         core.openflow.addListeners(self)
         core.openflow_discovery.addListeners(self)
+        core.host_tracker.addListenerByName("HostEvent", self._handle_HostEvent)
+        log.info('Controller initialized')
 
     def _handle_ConnectionUp(self, event):
         """
         Ref: https://noxrepo.github.io/pox-doc/html/#connectionup
         """
-        print('\nConnectionUp', event)
+        log.info("Switch %s has come up.", dpid_to_str(event.dpid))
 
     def _handle_ConnectionDown(self, event):
         """
         Ref: https://noxrepo.github.io/pox-doc/html/#connectiondown
         """
-        print('\nConnectionDown', event)
+        log.info("Switch %s has come down.", dpid_to_str(event.dpid))
+
+    def _handle_HostEvent(self, event):
+        """
+        Listen to host_tracker events, fired up every time a host is up or down
+        Fired up when a host make a ping to another.
+        host_tracker fires a pingall to make this events fire
+        Args:
+            event: HostEvent listening to core.host_tracker
+        """
+        log.info("Host %s has come up.", event.entry.macaddr.toStr())
+        # macaddr = event.entry.macaddr.toStr()
+        # port = event.entry.port
 
     def _handle_PortStatus(self, event):
         """Called when:
@@ -52,9 +68,14 @@ class FatTreeController:
         - A packet matches with FlowEntry "send to controller action".
         Ref: https://noxrepo.github.io/pox-doc/html/#packetin
         """
-        print('\nPacketIn', event)
+        packet = event.parsed
+        log.info("Packet arrived to switch %s:%s from %s to %s",
+                 dpid_to_str(event.dpid), event.port, packet.src, packet.dst)
 
     def _handle_LinkEvent(self, event):
+        """
+        Called when openflow_discovery discovers a new link
+        """
         if event.added:
             action = "added"
         elif event.removed:
@@ -62,9 +83,11 @@ class FatTreeController:
         else:
             action = "something else"
 
-        print('\nLinkEvent action: {}'.format(action), event)
-
+        link = event.link
+        log.info("Link has been %s from %s:%s to %s:%s", action, dpid_to_str(
+            link.dpid1), link.port1, dpid_to_str(link.dpid2), link.port2)
 
 def launch():
     core.registerNew(FatTreeController)
     pox.openflow.discovery.launch()
+    pox.host_tracker.launch()
