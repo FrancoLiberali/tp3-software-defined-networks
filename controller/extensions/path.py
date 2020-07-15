4daee408516a4c22cd862e9be7fab6d84b1ffc0f
Franco Liberali <franco.liberali@gmail.com>
2020-07-15 03:56:16 -0300

    fix inglish

--- controller/extensions/path.py
+++ controller/extensions/path.py
@@ -3,7 +3,7 @@ class Path(list):
     List of tuple (switch, port) to express which port output take in each switch,
     in order to get a destiny
     the first switch is the origin and the last is the destiny.
-    The port of the destiny whould be None because you allready are at destiny"""
+    The port destiny's should be None because you are at destiny already"""
     def add_jump(self, switch, port):
         self.append((switch, port))
     
