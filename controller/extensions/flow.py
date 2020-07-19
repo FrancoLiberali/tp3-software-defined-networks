f3655098d999eeeec3c5daca4e1dddb12b30d6c3
Francisco Penovi <francisco.penovi@gmail.com>
2020-07-19 13:44:20 -0300

    Changed srcport and dstport in flow object to None when protocol is ICMP

--- controller/extensions/flow.py
+++ controller/extensions/flow.py
@@ -14,6 +14,6 @@ class Flow:
     @staticmethod
     def of(ip_pkt):
         inner_pkt = ip_pkt.payload    # TCP, UDP or ICMP packet
-        src_port = inner_pkt.srcport if ip_pkt.protocol != ipv4.ICMP_PROTOCOL else 0   # Sets 0 to ports
-        dst_port = inner_pkt.dstport if ip_pkt.protocol != ipv4.ICMP_PROTOCOL else 0   # (ofp_match default) since ICMP
-        return Flow(ip_pkt.srcip, src_port, ip_pkt.dstip, dst_port, ip_pkt.protocol)   # doesn't have ports in header
+        src_port = inner_pkt.srcport if ip_pkt.protocol != ipv4.ICMP_PROTOCOL else None     # Sets 0 to ports
+        dst_port = inner_pkt.dstport if ip_pkt.protocol != ipv4.ICMP_PROTOCOL else None     # (ofp_match default) since ICMP
+        return Flow(ip_pkt.srcip, src_port, ip_pkt.dstip, dst_port, ip_pkt.protocol)        # doesn't have ports in header
